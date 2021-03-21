ced268d8d22c36417158732e37d8ccafdf1681be
Henrique <henrique1258@gmail.com>
2021-03-20 21:56:04 -0300

    Testando MariaDB e Java

--- src/com/testes/DatabaseTest.java
+++ src/com/testes/DatabaseTest.java
@@ -2,18 +2,11 @@ package com.testes;
 
 public class DatabaseTest {
     public static void main(String[] args) {
+        TestConnection m1 = new TestConnection();
+        CreateTable c1 = new CreateTable();
 
-        Database bancoDados = new Database();
-        DatabaseActions insertTest = new DatabaseActions();
+        m1.testConnection("exampledb");
+        c1.createNewTable("exampledb");
 
-        //Connect and create the table
-        bancoDados.connect();
-        bancoDados.createNewTable("Teste");
-
-        // insert three new rows
-        insertTest.insert("Rango", 100.00);
-        insertTest.insert("Asa", 200.00);
-        insertTest.insert("Sopa", 12.00);
-        insertTest.selectAll();
     }
 }
